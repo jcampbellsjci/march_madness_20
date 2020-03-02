b1f8f5d2aa6d798d4727a49534440ba4c4b4c2e6
Jake <jcampbell2@2u.com>
2020-03-02 16:04:12 -0500

    Renamed data files and made season summary for all teams

--- R/01_data_prep.R
+++ R/01_data_prep.R
@@ -1,8 +1,12 @@
 # Goal of this script is to just clean the data and put it into a
 # good modeling format.
 # 1. Season averages are found for each team for all box-score stats
-# 2. Find season outcome totals (wins and losses)
-# 3. Combine data and find shooting ratios
+# 2. Find record for season
+# 3. Find record for past five games
+# 4. Find record against tournament teams
+# 5. Clean Ken Pom data
+# 6. Combine data into a season summary
+# 7. Combine season summaries into a head-to-head tibble
 
 #### Data Load ####
 
@@ -18,7 +22,7 @@ tourney_results <- read_csv("data/MDataFiles_Stage1/MNCAATourneyCompactResults.c
 sample_sub <- read_csv("data/MSampleSubmissionStage1_2020.csv")
 
 
-#### Data Prep ####
+#### Find Box Score Averages ####
 
 # Finding the average box-score results for each team and each season
 # Data is separated by winners and losers
@@ -41,8 +45,17 @@ bs_avg <- rs_results %>%
   group_by(team1_TeamID, Season) %>%
   # Finding the mean of all counting stats
   summarize_at(.vars = vars(team1_Score, team2_Score, team1_FGM:team2_PF), 
-               .funs = ~ mean(., na.rm = T))
+               .funs = ~ mean(., na.rm = T)) %>%
+  # Calculating percentages
+  mutate(team1_FGP = team1_FGM / team1_FGA,
+         team1_FG3P = team1_FGM3 / team1_FGA3,
+         team1_FTP = team1_FTM / team1_FTA,
+         team2_FGP = team2_FGM / team2_FGA,
+         team2_FG3P = team2_FGM3 / team2_FGA3,
+         team2_FTP = team2_FTM / team2_FTA)
+  
 
+#### Find W-L Season Outcome ####
 
 # Creating a wins and losses dataframe for every team
 outcome <- rs_results %>%
@@ -52,7 +65,11 @@ outcome <- rs_results %>%
   # Joining number of times team had a losing ID
   inner_join(rs_results %>%
                group_by(LTeamID, Season) %>%
-               summarize(L = n()), by = c("WTeamID" = "LTeamID", "Season"))
+               summarize(L = n()), by = c("WTeamID" = "LTeamID", "Season")) %>%
+  mutate(WP = W / (W + L))
+
+
+#### Find W-L Over Past Five Games ####
 
 # Interested in finding wins and losses going into the tournament
 outcome_5 <- rs_results %>%
@@ -83,36 +100,41 @@ outcome_5 <- rs_results %>%
   mutate(WP_5 = W / (W + L)) %>%
   select(team1_TeamID, Season, WP_5)
 
-# Combining data sets into a season summary data set
-season_summary <- teams %>%
-  inner_join(seeds, by = "TeamID") %>%
-  left_join(outcome, by = c("TeamID" = "WTeamID", "Season")) %>%
-  left_join(outcome_5, by = c("TeamID" = "team1_TeamID", "Season")) %>%
-  left_join(bs_avg, by = c("TeamID" = "team1_TeamID", "Season")) %>%
-  rename_at(.vars = vars(starts_with("team1")), 
-            .funs = ~ gsub(pattern = "team1_", replacement = "", x = .)) %>%
-  rename_at(.vars = vars(starts_with("team2")), 
-            .funs = ~ gsub(pattern = "team2",
-                           replacement = "opponent", x = .)) %>%
-  # Calculating percentages
-  mutate(FGP = FGM / FGA, FG3P = FGM3 / FGA3, FTP = FTM / FTA,
-         opponent_FGP = opponent_FGM / opponent_FGA,
-         opponent_FG3P = opponent_FGM3 / opponent_FGA3,
-         opponent_FTP = opponent_FTM / opponent_FTA,
-         WP = W / (W + L)) %>%
-  # Getting seeds to be numeric
-  # Have to remove region character
-  mutate(Seed = substring(Seed, 2)) %>%
-  mutate(Seed = as.numeric(gsub(pattern = "a|b", replacement = "",
-                                x = Seed))) %>%
-  filter(Season >= 2000) %>%
-  filter(!(is.na(W)))
+
+#### Finding W-L Against Tournament Teams ####
+
+outcome_tourney <- rs_results %>%
+  # Rename all winners as 1 and all losers as 2
+  rename_at(.vars = vars(starts_with("W")),
+            .funs = ~ gsub(pattern = "W", replacement = "team1_", x = .)) %>%
+  rename_at(.vars = vars(starts_with("L")),
+            .funs = ~ gsub(pattern = "L", replacement = "team2_", x = .)) %>%
+  # Binding a tibble where we do the exact opposite
+  # This allows a team to be listed as 1 for wins AND losses
+  bind_rows(rs_results %>%
+              rename_at(.vars = vars(starts_with("W")),
+                        .funs = ~ gsub(pattern = "W",
+                                       replacement = "team2_", x = .)) %>%
+              rename_at(.vars = vars(starts_with("L")),
+                        .funs = ~ gsub(pattern = "L",
+                                       replacement = "team1_", x = .))) %>%
+  select(DayNum, Season, team1_TeamID, team2_TeamID, team1_Score,
+         team2_Score) %>%
+  mutate(outcome = ifelse(team1_Score - team2_Score > 0, "W", "L")) %>%
+  inner_join(seeds, by = c("team2_TeamID" = "TeamID", "Season")) %>%
+  group_by(team1_TeamID, Season, outcome) %>%
+  summarize(result = n()) %>%
+  spread(key = outcome, value = result) %>%
+  mutate_at(.vars = vars(W, L),
+            .funs = ~ ifelse(is.na(.), 0, .)) %>%
+  select(team1_TeamID, Season, W_tourney = W, L_tourney = L)
 
 
-#### Ken Pom Set Up ####
+#### Cleaning Ken Pom Data ####
 
 # Load up kpom data and team spellings
-kpom <- read_csv(file = "data/our_data/kenpom.csv")
+kpom <- read_csv(file = "data/our_data/kenpom.csv") %>%
+  select(year, name, adjo, adjd, adjt, luck, oppos, oppds, adjemn)
 spellings <- read_csv(file = "data/MDataFiles_Stage1/MTeamSpellings.csv")
 
 # Finding everyone in kpom who is not in the team name data
@@ -134,19 +156,45 @@ spellings <- tibble(old_name = sort(unique(no_names$name)),
                                  "texas a&m cc", "tex.-pan american",
                                  "texas rio grande valley",
                                  "winston-salem-state"
-                                 )) %>%
+                    )) %>%
   left_join(spellings, by = c("new_name" = "TeamNameSpelling")) %>%
   select(TeamNameSpelling = old_name, TeamID) %>%
   bind_rows(spellings)
 
 kpom <- kpom %>%
   mutate(name = tolower(name)) %>%
-  left_join(spellings, by = c("name" = "TeamNameSpelling"))
-season_summary <- season_summary %>%
-  left_join(kpom, by = c("TeamID", "Season" = "year"))
+  left_join(spellings, by = c("name" = "TeamNameSpelling")) %>%
+  select(-name)
 
 
-#### Setting up data in tourney format ####
+#### Combining Data Into a School's Season Summary ####
+
+# Combining data sets into a season summary data set
+season_summary <- teams %>%
+  left_join(outcome, by = c("TeamID" = "WTeamID")) %>%
+  left_join(outcome_5, by = c("TeamID" = "team1_TeamID", "Season")) %>%
+  left_join(outcome_tourney, by = c("TeamID" = "team1_TeamID", "Season")) %>%
+  left_join(bs_avg, by = c("TeamID" = "team1_TeamID", "Season")) %>%
+  left_join(seeds, by = c("TeamID", "Season")) %>%
+  left_join(kpom, by = c("TeamID", "Season" = "year")) %>%
+  rename_at(.vars = vars(starts_with("team1")), 
+            .funs = ~ gsub(pattern = "team1_", replacement = "", x = .)) %>%
+  rename_at(.vars = vars(starts_with("team2")), 
+            .funs = ~ gsub(pattern = "team2",
+                           replacement = "opponent", x = .)) %>%
+  # Getting seeds to be numeric
+  # Have to remove region character
+  mutate(Seed = substring(Seed, 2)) %>%
+  mutate(Seed = as.numeric(gsub(pattern = "a|b", replacement = "",
+                                x = Seed))) %>%
+  filter(Season >= 2000) %>%
+  filter(!(is.na(W))) %>%
+  select(TeamID:Season, order(colnames(.)))
+
+write_csv(season_summary, path = "data/our_data/season_summary.csv")
+
+
+#### Combining Data into a H2H Tourney Format ####
 
 winners <- tourney_results %>%
   inner_join(season_summary, by = c("WTeamID" = "TeamID", "Season")) %>%
@@ -181,7 +229,9 @@ losers <- tourney_results %>%
 final <- winners %>%
   bind_rows(losers %>%
               filter(!(paste0(team1_TeamID, team2_TeamID) %in%
-                         paste0(winners$team2_TeamID, winners$team1_TeamID))))
+                         paste0(winners$team2_TeamID,
+                                winners$team1_TeamID)))) %>%
+  select(Season:NumOT, order(colnames(.)))
   
 
-write_csv(season_summary, path = "data/our_data/starter_file.csv")
+write_csv(season_summary, path = "data/our_data/tourney_h2h.csv")
